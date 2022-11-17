4fbbe3038febec5349f0163229aa5f67475f8408
isaacalabdi1998 <88029170+isaacalabdi1998@users.noreply.github.com>
2022-11-17 12:15:10 +0300

    create Python Data Types.py

---
+++ Image Processing/2-week/Python Data Types.py
@@ -0,0 +1,24 @@
+
+#----------------------
+# Python Data Types
+#----------------------
+
+print(type(1998) )  # int => Integer
+
+print(type(-1998) ) # int => Integer
+
+print(type(1998.5) )    # int => Integer
+
+print(type('$') )   # str => String
+
+print(type("my name is") )  # str => String
+
+print(type( [1, 2, 3, 4]) ) # list  => List
+
+print(type( (1, 2, 3, 4)) ) # tuple => Tuple
+
+print(type( {"one": 1, "Two":2, "Three":3} ) )  # dict => Dictionary
+
+print(type(2 == 2) )  # bool => Boolean
+
+
