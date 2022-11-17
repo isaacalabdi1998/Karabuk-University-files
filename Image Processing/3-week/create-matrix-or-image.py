d5a8fa0847540ab72ea4b1926cc09af760278e2b
isaacalabdi1998 <88029170+isaacalabdi1998@users.noreply.github.com>
2022-11-17 12:45:12 +0300

    create create-matrix-or-image.py

---
+++ Image Processing/3-week/create-matrix-or-image.py
@@ -0,0 +1,14 @@
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+row = 256
+col = 256
+img =np.zeros((row, col))
+img[100:105, :] = 0.5
+img[:, 100:105] = 1
+
+plt.figure(figsize=(10,4))
+plt.imshow(img)
+plt.show()
